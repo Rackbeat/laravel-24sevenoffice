59b3fcb7a5f01cca5b899ae77dd0b604dc1ce164
Stefan Ninic <ninicstefan94@gmail.com>
2020-08-20 16:05:50 +0200

    Finishing rate limit

--- src/config/laravel-24so.php
+++ src/config/laravel-24so.php
@@ -7,4 +7,5 @@ return [
 	'api_key'    => env( 'LARAVEL_24SO_API_KEY' ),
 	'shop_url'   => env( 'LARAVEL_24SO_SHOP_URL' ),
 	'user_agent' => '4SevenOffice API Wrapper (https://github.com/kg-bot/laravel-24sevenoffice)',
+	'api_limit'  => env( 'LARAVEL_24SO_RATE_LIMIT', 7200 ),
 ];
