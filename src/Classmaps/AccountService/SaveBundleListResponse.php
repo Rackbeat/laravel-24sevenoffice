b892c0a2f23fa7b5e6b47e9c8e3e9035500dd0fa
Stefan Ninic <ninicstefan94@gmail.com>
2021-01-26 15:01:19 +0100

    Fixed inventory count return type

--- src/Classmaps/AccountService/SaveBundleListResponse.php
+++ src/Classmaps/AccountService/SaveBundleListResponse.php
@@ -4,17 +4,13 @@
 namespace KgBot\SO24\Classmaps\AccountService;
 
 
-use KgBot\SO24\Classmaps\AuthenticateService\Client;
 use KgBot\SO24\Classmaps\BaseResponse;
 
 class SaveBundleListResponse extends BaseResponse
 {
 	protected $resultsName = 'SaveBundleListResult';
 
-	/**
-	 * @return Client
-	 */
-	public function getResults(): Client {
+	public function getResults() {
 		return $this->{$this->resultsName};
 	}
 }
